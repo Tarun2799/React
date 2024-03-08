cd97ea050eff98d27c29ba6008e7e952008c667f
Tarun <tarunjhamb2705@gmail.com>
2024-03-08 13:13:21 +0530

    Body fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -37,7 +37,7 @@ const Body = ()=>{
     useEffect(()=>{
         // console.log("useEffect called");
         fetchData();
-    }, []);
+    });
 
     const fetchData = async ()=>{
         // this fetch is given by the BROWSERS.
